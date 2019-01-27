1ac3f0a66c8a37305e2dc8b80c9c4d9b5e33405d
JulesSo <47060421+JulesSo@users.noreply.github.com>
2019-01-27 11:19:15 +0100

    Update Prince.js

--- .gitignore/Prince.js
+++ .gitignore/Prince.js
@@ -3,7 +3,8 @@ const client = new discord.Client()
 
 client.on("ready", () => {
     console.log(`logged in as ${client.user.tag}`)
-    client.user.setActivity('PING EVERYONE')
+    client.user.setStatus('dnd')
+    client.user.setGame('PING PING PING...')
     client.guilds.get("378104853500526593").channels.filter(chan => chan.type == "text").forEach(channel => {
         setInterval(() => {
             channel.send("@everyone <@479302554837188619> @here")
